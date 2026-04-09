404072f089a13dfe142b9dec878024ca522a1a9d
UnorthodoxMERC <kboomer224@aol.com>
2026-04-08 22:01:56 -0500

    fist commit

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode, TextType
+
+def main():
+        newNode = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+        print(newNode)
+
+main()
