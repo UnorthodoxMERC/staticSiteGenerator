cc0d9b23756da8ce9b818dd470e080eeba2f7266
UnorthodoxMERC <kboomer224@aol.com>
2026-04-09 21:51:47 -0500

    added a LeafNode class that inherits from HTMLNode and unit tests for LeafNode class

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest 
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_one_prop(self):
@@ -14,6 +14,24 @@ class TestHTMLNode(unittest.TestCase):
     def test_mult_props(self):
         node = HTMLNode(None, None, None, {'href': 'https://wwww.boot.dev', 'target': '_blank'})
         self.assertEqual(node.props_to_html(), ' href="https://wwww.boot.dev" target="_blank"')
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html_pTag(self):
+        node = LeafNode("p", "some value")
+        self.assertEqual(node.to_html(), '<p>some value</p>')
+
+    def test_to_html_with_props(self):
+        node = LeafNode("a", "some value", {"href": "https://www.boot.dev"})
+        self.assertEqual(node.to_html(), '<a href="https://www.boot.dev">some value</a>')
+
+    def test_none_tag(self):
+        node = LeafNode(None, 'some value')
+        self.assertEqual(node.to_html(), 'some value')
+
+    def test_none_value(self):
+        node = LeafNode(None, None)
+        with self.assertRaises(ValueError):
+            node.to_html()
 
 if __name__ == "__main__":
     unittest.main()
