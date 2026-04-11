13d624bba53ff326d995ac3cd1c353c9e8c0d482
UnorthodoxMERC <kboomer224@aol.com>
2026-04-10 20:50:16 -0500

    added a function to convert a TextNode into a LeafNode and unit test for the new func

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -18,6 +18,39 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("test node", TextType.TEXT, "www.boomer.com")
         node2 = TextNode("test node", TextType.TEXT)
         self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode('This is a text node', TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, 'This is a text node')
+
+    def test_bold(self):
+        node = TextNode('this is a bold node', TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'b')
+        self.assertEqual(html_node.value, 'this is a bold node')
+
+    def test_link(self):
+        node = TextNode('babam', TextType.LINK, 'https://www.babam.com')
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'a')
+        self.assertEqual(html_node.value, 'babam')
+        self.assertEqual(html_node.props, {'href': 'https://www.babam.com'})
+
+    def test_image(self):
+        node = TextNode('alt text', TextType.IMAGE, 'https://www.babam.com')
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'img')
+        self.assertEqual(html_node.value, '')
+        self.assertEqual(html_node.props, {'src': 'https://www.babam.com', 'alt': 'alt text'})
+
+    def test_wrong_TextType(self):
+        node = TextNode('blah blah', 'blah')
+        with self.assertRaises(ValueError):
+            html_node = text_node_to_html_node(node)
+
+
 
 
 if __name__ == "__main__":
