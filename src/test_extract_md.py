4795519eb547f2bcb93944a2817a5fc372efb7bc
UnorthodoxMERC <kboomer224@aol.com>
2026-04-12 21:51:29 -0500

    added funcs to extract md images and links, and unit test on those funcs

---
+++ src/test_extract_md.py
@@ -0,0 +1,16 @@
+import unittest
+
+from extract_md import extract_markdown_links, extract_markdown_images
+
+class TestExtractMd(unittest.TestCase): 
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with a ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_link(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
